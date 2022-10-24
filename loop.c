dc8c2cbfc4e16a0d5c6c602d5dcc5477fe3375ec
HIMANSHU KUMAR <104679475+himanshu-278@users.noreply.github.com>
2022-10-24 11:19:29 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num,tNum,digit,cnt;
+    int rem;
+ 
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    printf("Enter digit to search: ");
+    scanf("%d",&digit);
+ 
+    cnt=0;
+    tNum=num;
+ 
+    while(tNum>0)
+    {
+        rem=tNum%10;
+        if(rem==digit)
+            cnt++;
+            
+        tNum/=10;
+    }
+    printf("digits founds in the entered number is : %d in number : %d.",cnt,num);
+    return 0;
+    }
